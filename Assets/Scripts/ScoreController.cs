f155337a4f53e6729a0ce3bcf7280391ef620168
Abdul003 <73937383+nemesisx18@users.noreply.github.com>
2022-08-05 10:10:00 +0700

    Adding UI & Game Manager

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreController : MonoBehaviour
+{
+    public TextMeshProUGUI skorKiri;
+    public TextMeshProUGUI skorKanan;
+
+    public ScoreManager manager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        skorKiri.text = manager.leftScore.ToString();
+        skorKanan.text = manager.rightScore.ToString();
+    }
+}
